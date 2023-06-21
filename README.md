00d6a36cb568fca72dc505b9836ec6643b8c99d3
James <jameslomax96@gmail.com>
2023-06-21 14:08:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+TO be updated
+
+# DTS User Guide
+
+Congrats! You just saved yourself hours of work by bootstrapping this project with DTS. Let’s get you oriented with what’s here and how to use it.
+
+> This DTS setup is meant for developing libraries (not apps!) that can be published to NPM. If you’re looking to build a Node app, you could use `ts-node-dev`, plain `ts-node`, or simple `tsc`.
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import cron from 'node-cron';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Retrieve routes for crawler
+const mojangRoute = process.env.CRAWL_MOJANG_ROUTE;
+const serverListRoute = process.env.CRAWL_SERVERLIST_ROUTE;
+if (!mojangRoute || !serverListRoute) {
+  throw new Error('Routes are not defined in .ENV');
+}
+
+// Secret password to use crawler rotues
+const secretPassword = process.env.SECRET;
+if (!secretPassword) {
+  throw new Error('Secret is not defined in .ENV');
+}
+
+/**
+ * === Node scheduler ===
+ */
+
+/** Mojang API */
+cron.schedule('*/5 * * * *', async () => {
+  console.log('Running Mojang crawler schedule (every 5 mins)');
+  try {
+    await axios.post(`${mojangRoute}?password=${secretPassword}`);
+  } catch (error) {
+    console.error('Mojang crawl failed:', error);
+  }
+});
+
+/** Mojang API */
+cron.schedule('0 2 * * *', async () => {
+  console.log('Running ServerList crawler schedule (at 2am each day)');
+  try {
+    await axios.post(`${serverListRoute}?password=${secretPassword}`);
+  } catch (error) {
+    console.error('ServerList crawl failed:', error);
+  }
+});
