2c72423f87774836835e0aa720383b8a44514174
James <jameslomax96@gmail.com>
2023-06-29 13:48:10 +0100

    add: sweeper and quickUpdate routes. add back in serverlist crawl at 2am

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Retrieve routes for crawler
-const mojangRoute = process.env.CRAWL_MOJANG_ROUTE;
-const serverListRoute = process.env.CRAWL_SERVERLIST_ROUTE;
-if (!mojangRoute || !serverListRoute) {
+const quickUpdateRoute = process.env.CRAWL_MOJANG_ROUTE;
+const sweeperRoute = process.env.MOJANG_QUICKUPDATE_ROUTE;
+const serverListRoute = process.env.MOJANG_SWEEPER_ROUTE;
+
+if (!quickUpdateRoute || !serverListRoute || !sweeperRoute) {
   throw new Error('Routes are not defined in .ENV');
 }
 
@@ -23,10 +25,28 @@ if (!secretPassword) {
 console.info('⌛ Now running scheduler');
 
 /** Mojang API */
-cron.schedule('0 * * * *', async () => {
-  console.log('Running Mojang crawler schedule (every hour)');
+cron.schedule('*/5 * * * *', async () => {
+  console.log('Running Mojang quickUpdate schedule (every 5 minutes)');
   try {
-    await axios.post(`${mojangRoute}?password=${secretPassword}`);
+    await axios.post(`${quickUpdateRoute}?password=${secretPassword}`);
+  } catch (error) {
+    console.error('Mojang crawl failed:', error);
+  }
+});
+
+cron.schedule('*/15 * * * *', async () => {
+  console.log('Running Mojang sweeper schedule (every 15 minutes)');
+  try {
+    await axios.post(`${quickUpdateRoute}?password=${secretPassword}`);
+  } catch (error) {
+    console.error('Mojang crawl failed:', error);
+  }
+});
+
+cron.schedule('0 2 * * *', async () => {
+  console.log('Running ServerList schedule (daily at 2am)');
+  try {
+    await axios.post(`${sweeperRoute}?password=${secretPassword}`);
   } catch (error) {
     console.error('Mojang crawl failed:', error);
   }
